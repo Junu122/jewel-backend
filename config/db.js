5061e2b0a50f20aacab095662b72426590ea9e3d
junaid <junaid.sheikh.1800@gmail.com>
2025-05-10 14:44:28 +0530

    added database connection

--- config/db.js
+++ config/db.js
@@ -1,1 +1,9 @@
 import mongoose from "mongoose";
+
+export const DBconnection=async()=>{
+      await mongoose.connect(process.env.DATABASE_URL).then(()=>{
+        console.log("database connected")
+    }).catch((err)=>{
+       console.log(err,"error occured")
+    })
+}
